a9b76514b8b6399aac47acca334b2ff2aa884847
Achal <tiwariachal@gmail.com>
2024-06-16 18:33:16 +0530

    basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
 
 function App() {
-
+  const title = 'TITLE'
+  const id = "cp-1"
+  const urgency = "Urgent"
+  const desc = "To do by tomorrow"
   return (
-    <div className="text-4xl text-red-500">
-      Hello
+    <div className="border rounded-lg px-2 m-2 bg-gray-50">
+      <div className='text-base font-semibold py-2'>
+        {title}
+      </div>
+      <div className="text-xs">
+        {desc}
+      </div>
+      <div className='text-xs flex justify-between py-2 text-gray-500'>
+        <div>{id}</div>
+        <div>{urgency}</div> 
+      </div>
     </div>
   )
 }
