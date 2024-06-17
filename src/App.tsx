4681ba83a53a2d462c990d6612bb1f8ab17829c4
Achal <tiwariachal@gmail.com>
2024-06-17 11:17:53 +0530

    hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status);
     return {
-      title: status,
+      status,
       tasks: tasksInColumn,
     };
   });
@@ -24,30 +24,39 @@ function App() {
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
     e.preventDefault()
+    setCurrentHover(null)
     const id = e.dataTransfer.getData("id")
-    const task = tasks.find((task) => task.id ===id)
-    if(task){
-      updateTask({...task, status})
+    const task = tasks.find((task) => task.id === id)
+    if (task) {
+      updateTask({ ...task, status })
     }
+  }
+
+  const [currentHover, setCurrentHover] = useState<Status | null>(null)
+  const handleDragEnter = (status: Status) => {
+    setCurrentHover(status)
   }
 
   return (
     <>
       <div className='flex divide-x'>
         {columns.map((column) => (
-          <div 
-          onDrop = {(e) => handleDrop(e, column.title)}
-          onDragOver={(e) => e.preventDefault()} 
-          className='w-1/3 px-2' 
-          key={column.title}
+          <div
+            onDrop={(e) => handleDrop(e, column.status)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={() => handleDragEnter(column.status)}
+            className='w-1/3 px-2'
+            key={column.status}
           >
-            <h1 className='text-2xl capitalize p-2 font-bold text-gray-600'>{column.title}</h1>
-            {column.tasks.map((task) => (
-              <TaskCard
-                task={task}
-                updateTask={updateTask}
-              />
-            ))}
+            <h1 className='text-2xl capitalize p-2 font-bold text-gray-600'>{column.status}</h1>
+            <div className={`h-full ${currentHover === column.status ? 'bg-gray-200' : ''}`}>
+              {column.tasks.map((task) => (
+                <TaskCard
+                  task={task}
+                  updateTask={updateTask}
+                />
+              ))}
+            </div>
           </div>
         ))}
       </div>
